ac2b67c4bcdb7dc2f022c4d84c789dd3d83b3b85
damzymichael <105460672+damzymichael@users.noreply.github.com>
2023-08-09 10:54:19 +0100

    first commit

---
+++ utils.js
@@ -0,0 +1,10 @@
+//debounce function to delay a function
+const debounce = (func, delay = 1500) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
